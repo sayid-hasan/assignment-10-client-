774fd4151b6bb53a4ffe1c6cc78130826a6df458
mohammad syed hasan <syedhasanmohammad@gmail.com>
2024-04-28 18:08:59 +0600

    added banner section with info

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -1,6 +1,9 @@
 //import React, { useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 // Import Swiper styles
 import "swiper/css";
@@ -13,6 +16,9 @@ import "./banner.css";
 import { Pagination, Navigation } from "swiper/modules";
 
 const Banner = () => {
+  useEffect(() => {
+    AOS.init({ duration: "700", delay: "700" });
+  }, []);
   return (
     <Swiper
       slidesPerView={1}
@@ -27,17 +33,54 @@ const Banner = () => {
     >
       <SwiperSlide>
         <div>
-          <img src={"https://i.ibb.co/7gpNX6T/carousel-image-1.jpg"} alt="" />
+          <div className="relative" data-aos="fade-right">
+            <img src={"https://i.ibb.co/XYvhyD4/carousel-image-1.jpg"} alt="" />
+            <div className="absolute bottom-1 md:top-1/2 md:-translate-y-1/2 rounded-lg md:left-12  glass p-4 md:max-w-[450px] max-w-[200px] ">
+              <h2 className="text-[#102E56] md:text-2xl text-xl  font-jacquard">
+                Serenity in Shades: Mountain Lake Landscape Painting
+              </h2>
+              <p className="font-roboto mt-2 text-sm md:text-base text-white text-opacity-45">
+                Capturing the essence of a peaceful mountain scene reflected in
+                the clear waters of a serene lake, this artwork evokes a sense
+                of calm and tranquility.{" "}
+              </p>
+            </div>
+          </div>
         </div>
       </SwiperSlide>
       <SwiperSlide>
-        <div>
+        <div className="relative">
           <img src={"https://i.ibb.co/CJvRBQH/carousel-image-3.jpg"} alt="" />
+          <div className="absolute bottom-1 md:top-1/2 md:-translate-y-1/2 rounded-lg md:left-12  glass p-4 md:max-w-[450px] max-w-[200px] ">
+            <h2 className="text-[#FC736B] md:text-2xl text-xl  font-jacquard">
+              Whispers of Eternity: Enigmatic Night Sky Oil Painting
+            </h2>
+            <p className="font-roboto mt-2 text-sm md:text-base text-white text-opacity-45 mb-2">
+              Embark on a journey into the depths of the cosmos with our
+              mesmerizing &apos;Whispers of Eternity&apos; oil painting. Against
+              a canvas of infinite darkness, vibrant hues of celestial bodies
+              dance in a symphony of light and shadow, illuminating the night
+              sky with ethereal beauty..{" "}
+            </p>
+          </div>
         </div>
       </SwiperSlide>
       <SwiperSlide>
-        <div>
+        <div className="relative">
           <img src={"https://i.ibb.co/NYPwQny/carousel-image-2.jpg"} alt="" />
+          <div className="absolute bottom-1 md:top-1/2 md:-translate-y-1/2 rounded-lg md:left-12  glass p-4 md:max-w-[450px] max-w-[200px] ">
+            <h2 className="text-[#E17630] md:text-2xl text-xl  font-jacquard">
+              Autumn&#39;s Grace: Portrait of a Radiant Maiden
+            </h2>
+            <p className="font-roboto mt-2 text-sm md:text-base text-white text-opacity-45 mb-2">
+              Immerse yourself in the enchanting allure of &#39;Autumn&#39;s
+              Grace&#39;' a captivating portrait capturing the beauty and
+              serenity of a radiant maiden amidst the vibrant hues of fall
+              foliage. With a gentle gaze that mirrors the tranquility of the
+              forest around her, she stands as a symbol of elegance and
+              harmony..{" "}
+            </p>
+          </div>
         </div>
       </SwiperSlide>
     </Swiper>
