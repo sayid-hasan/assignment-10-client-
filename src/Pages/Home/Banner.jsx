fff50440909990074da2bd508c90d2ba258f9c11
mohammad syed hasan <syedhasanmohammad@gmail.com>
2024-04-28 21:26:48 +0600

    made banner section fully responsive

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -33,13 +33,13 @@ const Banner = () => {
     >
       <SwiperSlide>
         <div>
-          <div className="relative" data-aos="fade-right">
+          <div className="flex  flex-col md:relative" data-aos="fade-right">
             <img src={"https://i.ibb.co/XYvhyD4/carousel-image-1.jpg"} alt="" />
-            <div className="absolute bottom-1 md:top-1/2 md:-translate-y-1/2 rounded-lg md:left-12  glass p-4 md:max-w-[450px] max-w-[200px] ">
+            <div className="block max-w-full md:absolute bottom-1 lg:top-1/2 lg:-translate-y-1/2 rounded-lg lg:left-12  glass lg:p-4 md:p-2 md:max-w-full md:bottom-0  lg:max-w-[450px]  ">
               <h2 className="text-[#102E56] md:text-2xl text-xl  font-jacquard">
                 Serenity in Shades: Mountain Lake Landscape Painting
               </h2>
-              <p className="font-roboto mt-2 text-sm md:text-base text-white text-opacity-45">
+              <p className="font-roboto mt-2 text-sm md:text-base text-black md:text-white text-opacity-45">
                 Capturing the essence of a peaceful mountain scene reflected in
                 the clear waters of a serene lake, this artwork evokes a sense
                 of calm and tranquility.{" "}
@@ -49,13 +49,13 @@ const Banner = () => {
         </div>
       </SwiperSlide>
       <SwiperSlide>
-        <div className="relative">
+        <div className="flex  flex-col md:relative">
           <img src={"https://i.ibb.co/CJvRBQH/carousel-image-3.jpg"} alt="" />
-          <div className="absolute bottom-1 md:top-1/2 md:-translate-y-1/2 rounded-lg md:left-12  glass p-4 md:max-w-[450px] max-w-[200px] ">
+          <div className="block max-w-full md:absolute bottom-1 lg:top-1/2 lg:-translate-y-1/2 rounded-lg lg:left-12  glass lg:p-4 md:p-2 md:max-w-full md:bottom-0  lg:max-w-[450px]  ">
             <h2 className="text-[#FC736B] md:text-2xl text-xl  font-jacquard">
               Whispers of Eternity: Enigmatic Night Sky Oil Painting
             </h2>
-            <p className="font-roboto mt-2 text-sm md:text-base text-white text-opacity-45 mb-2">
+            <p className="font-roboto mt-2 text-sm md:text-base text-black md:text-white text-opacity-45">
               Embark on a journey into the depths of the cosmos with our
               mesmerizing &apos;Whispers of Eternity&apos; oil painting. Against
               a canvas of infinite darkness, vibrant hues of celestial bodies
@@ -66,13 +66,13 @@ const Banner = () => {
         </div>
       </SwiperSlide>
       <SwiperSlide>
-        <div className="relative">
+        <div className="flex  flex-col md:relative">
           <img src={"https://i.ibb.co/NYPwQny/carousel-image-2.jpg"} alt="" />
-          <div className="absolute bottom-1 md:top-1/2 md:-translate-y-1/2 rounded-lg md:left-12  glass p-4 md:max-w-[450px] max-w-[200px]   ">
+          <div className="block max-w-full md:absolute bottom-1 lg:top-1/2 lg:-translate-y-1/2 rounded-lg lg:left-12  glass lg:p-4 md:p-2 md:max-w-full md:bottom-0  lg:max-w-[450px]  ">
             <h2 className="text-[#E17630] md:text-2xl text-xl  font-jacquard">
               Autumn&#39;s Grace: Portrait of a Radiant Maiden
             </h2>
-            <p className="font-roboto mt-2 text-sm md:text-base text-white text-opacity-45 mb-2">
+            <p className="font-roboto mt-2 text-sm md:text-base text-black md:text-white text-opacity-45">
               Immerse yourself in the enchanting allure of &#39;Autumn&#39;s
               Grace&#39; a captivating portrait capturing the beauty and
               serenity of a radiant maiden amidst the vibrant hues of fall
